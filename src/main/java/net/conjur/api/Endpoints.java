02fd5ab995bd3bf4641fb1fbe076420c90f635de
Jon <jonathan.j.mason@gmail.com>
2013-09-25 14:16:01 -0500

    add endpoints configuration

---
+++ src/main/java/net/conjur/api/Endpoints.java
@@ -0,0 +1,64 @@
+package net.conjur.api;
+
+public class Endpoints {
+	public static final int AUTHN_PORT = 5000;
+	public static final int AUTHZ_PORT = 5100;
+	public static final int DIRECTORY_PORT = 5200;
+	
+	private final String environment;
+	
+	private final String stack;
+	
+	private Endpoints(String environment, String stack){
+		this.environment = environment;
+		this.stack = stack;
+	}
+	
+	public String authn(String account){
+		return Endpoints.authnUrl(environment, stack, account);
+	}
+	
+	public String authz(String account){
+		return Endpoints.authzUrl(environment, stack, account);
+	}
+	
+	public String directory(String account){
+		return Endpoints.directoryUrl(environment, stack, account);
+	}
+	
+	// I can't think of a decent name for this method...
+	public static Endpoints of(String environment, String stack){
+		return new Endpoints(environment, stack);
+	}
+	
+	public static String authnUrl(String environment, String stack, String account){
+		if(useLocalhost(environment)){
+			return localEndpoint(AUTHN_PORT);
+		}
+		return String.format("https://authn-%s-conjur.herokuapp.com",account);
+	}
+	
+	public static String authzUrl(String environment, String stack, String account){
+		if(useLocalhost(environment)){
+			return localEndpoint(AUTHZ_PORT);
+		}
+		return String.format("https://authz-%s-conjur.herokuapp.com",stack);
+	}
+	
+	public static String directoryUrl(String environment, String stack, String account){
+		if(useLocalhost(environment)){
+			return localEndpoint(DIRECTORY_PORT);
+		}
+		return String.format("https://core-%s-conjur.herokuapp.com",account);
+	}
+
+	private static boolean useLocalhost(String environment){
+		if(environment == null)
+			throw new NullPointerException();
+		return "test".equals(environment) || "development".equals(environment);
+	}
+	
+	private static String localEndpoint(int port){
+		return String.format("http://localhost:%d", port);
+	}
+}
