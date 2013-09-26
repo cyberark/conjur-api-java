fe187a869cdc80b5219fdacd1c584c6481812f3f
Jon <jonathan.j.mason@gmail.com>
2013-09-26 14:59:23 -0500

    authn finished needs tests

--- src/main/java/net/conjur/api/Client.java
+++ src/main/java/net/conjur/api/Client.java
@@ -1,5 +1,102 @@
 package net.conjur.api;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 public abstract class Client {
+	private CloseableHttpClient httpClient;
+	
 	protected abstract String getEndpoint();
+	
+	/**
+	 * Return a URI for our endpoint.
+	 * @return a URI for our endpoint
+	 */
+	protected URI getURI(){
+		return URI.create(getEndpoint());
+	}
+	
+	
+	protected String requestString(CloseableHttpClient client, HttpUriRequest request) throws IOException, HttpStatusException {
+		try{
+			CloseableHttpResponse response = client.execute(request);
+			try{
+				HttpStatusException.throwErrors(response);
+				if(response.getEntity() == null)
+					return "";
+				return EntityUtils.toString(response.getEntity());
+			}finally{
+				response.close();
+			}
+		}finally{
+			client.close();
+		}
+	}
+	
+	/**
+	 * Create an HttpClient configured to use basic auth
+	 * @param username Basic auth username
+	 * @param password Basic auth password
+	 * @return An HttpClient instance that will authenticate as the given username/password.
+	 */
+	protected CloseableHttpClient httpClientWithBasicAuth(String username, String password){
+		return HttpClients.custom()
+				.setDefaultCredentialsProvider(credentialsFor(username, password))
+				.build();
+	}
+	
+	/**
+	 * Return a (cached) default {@link CloseableHttpClient}
+	 */
+	protected CloseableHttpClient defaultHttpClient(){
+		if(httpClient == null){
+			httpClient = HttpClients.createDefault();
+		}
+		return  httpClient;
+	}
+	
+	/**
+	 * Create a {@link CredentialsProvider} for basic auth 
+	 * with the given username and password.
+	 * @param username basic auth username
+	 * @param password basic auth password
+	 * @return a {@link CredentialsProvider} configured to authenticate as the given identity.
+	 */
+	protected CredentialsProvider credentialsFor(String username, String password){
+		URI uri = getURI();
+		CredentialsProvider creds = new BasicCredentialsProvider();
+		creds.setCredentials(new AuthScope(uri.getHost(), uri.getPort()), 
+				new UsernamePasswordCredentials(username, password));
+		return creds;
+	}
+	
+	/**
+	 * Encode a URL component.
+	 * @param uriComponent
+	 * @return
+	 */
+	public static String fullyEscape(String uriComponent){
+		try {
+			// SO...Lets discuss this!
+			// api-ruby uses CGI.escape, and the JRuby implementation of that method uses URLEncoder, 
+			// so I think this is good.  However, the api-node implementation uses encodeURIComponent,
+			// which is *not* exactly the same!  See this stack overflow question: http://tinyurl.com/nmbu4ge.
+			// Not sure what to do about this.
+			return URLEncoder.encode(uriComponent, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("WTF? UTF-8 encoding is apparently unsupported??", e);
+		}
+	}
 }
--- src/main/java/net/conjur/api/authn/Authn.java
+++ src/main/java/net/conjur/api/authn/Authn.java
@@ -1,18 +1,67 @@
 package net.conjur.api.authn;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import net.conjur.api.Client;
+import net.conjur.api.ConjurApiException;
+import net.conjur.api.Endpoints;
+import net.conjur.api.HttpStatusException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 
 public class Authn extends Client {
 
+	private String endpoint;
+	
+	public Authn(String endpoint){
+		this.endpoint = endpoint;
+	}
+	
+	public Authn(URI endpoint){
+		this(endpoint.toString());
+	}
+	
+	public Authn(Endpoints endpoints, String account){
+		this(endpoints.authn(account));
+	}
+	
 	/**
 	 * Exchange a username/apiKey pair for a token that can be used to authenticate
 	 * future API calls.
 	 * @param username a conjur username
 	 * @param apiKey an api key as returned by {@link #authenticate(String, String)}
-	 * @return a {@link Token} that can be used to authenticate api calls.
+	 * @return a base64 encoded string that can be used to authenticate api calls.
 	 */
-	public Token authenticate(String username, String apiKey){
-		
+	public String authenticate(String username, String apiKey) throws ConjurApiException{
+		try{
+			return authenticateThrowing(username, apiKey);
+		}catch(IOException e){
+			throw new ConjurApiException(e);
+		} catch (URISyntaxException e) {
+			throw new ConjurApiException(e);
+		}catch(HttpStatusException e){
+			if(e.getStatusCode() == 401){
+				throw  new AuthenticationFailure(e);
+			}
+			throw new ConjurApiException(e);
+		}
+	}
+	
+	private String authenticateThrowing(String username, String apiKey) throws IOException, HttpStatusException, URISyntaxException{
+		CloseableHttpClient client = defaultHttpClient();
+		URI uri = new URIBuilder(getURI()).setPath("users/" + fullyEscape(username) + "/authenticate")
+				.build();
+		HttpPost post = new HttpPost(uri);
+		post.setHeader("Content-Type", "text/plain");
+		post.setEntity(new StringEntity(apiKey));
+		return Base64.encodeBase64URLSafeString(requestString(client, post).getBytes());
 	}
 	
 	/**
@@ -20,16 +69,35 @@ public class Authn extends Client {
 	 * @param username the conjur username
 	 * @param password the password for the given username
 	 * @return an API key
-	 * @throws TODO what does this throw?
+	 * @throws ConjurApiException when something goes wrong other than a failed login
 	 */
-	public String login(String username, String password){
-		
+	public String login(String username, String password) throws ConjurApiException{
+		try{
+			return loginThrowing(username, password);
+		}catch(IOException e){
+			throw new ConjurApiException(e);
+		}catch(URISyntaxException e){
+			throw new ConjurApiException(e);
+		}catch(HttpStatusException e){
+			if(e.getStatusCode() == 401){
+				throw new AuthenticationFailure(e);
+			}else{
+				throw new ConjurApiException(e);
+			}
+		}
+	}
+	
+	private String loginThrowing(String username, String password) throws IOException, URISyntaxException, HttpStatusException{
+		CloseableHttpClient client = httpClientWithBasicAuth(username, password);
+		URI uri = new URIBuilder(getURI()).setPath("users/login").build();
+		HttpGet get = new HttpGet(uri);
+		return requestString(client, get);
 	}
 	
 	@Override
 	protected String getEndpoint() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.endpoint;
 	}
+
 
 }
