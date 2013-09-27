f36b536ac56b68b36bffd4f19a8a1ecf8b086be6
Jon <jonathan.j.mason@gmail.com>
2013-09-27 13:26:29 -0500

    turn off debug logging in example

--- bin/basic-usage.sh
+++ bin/basic-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ARGS="-classpath %classpath"
 # Uncomment the next line for verbose output
-ARGS="$ARGS -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commmons.logging.simplelog.showdatetime=true -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG"
+#ARGS="$ARGS -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commmons.logging.simplelog.showdatetime=true -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG"
 if [[ -z $CONJUR_AUTHN ]] ; then
 	echo "You must set CONJUR_AUTHN to username:password"
 	exit 1
