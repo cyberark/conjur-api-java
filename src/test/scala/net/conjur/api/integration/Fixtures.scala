49355b129d87e0f2407630a1202448b7d355552b
Jon <jonathan.j.mason@gmail.com>
2013-10-03 16:50:50 -0500

    Integration tests

--- src/test/scala/net/conjur/api/integration/Fixtures.scala
+++ src/test/scala/net/conjur/api/integration/Fixtures.scala
@@ -1,8 +1,85 @@
 package net.conjur.api.integration
 
-/**
- *
- */
-class Fixtures {
+import scala.collection.mutable
+import net.conjur.api.{User, Credentials, Conjur}
+import org.scalatest._
+import net.conjur.api.authn.CachingAuthnProvider
+import scala.util.Random
 
+trait RandomStringLike {
+  def ++ = next
+  def next:String
+}
+
+object RandomString extends RandomStringLike {
+  private class Impl(prefix:String, length:Int) extends RandomStringLike {
+    def next = prefix + List.fill(length)(Random.nextPrintableChar()).mkString
+  }
+
+  private lazy val impl = new Impl("", 6)
+
+  def apply(prefix:String, length:Int):RandomStringLike = new Impl(prefix, length)
+  def apply(prefix:String):RandomStringLike = new Impl(prefix, 16)
+  def apply(length:Int):RandomStringLike = new Impl("", length)
+  def apply():RandomStringLike = new Impl("", 6)
+
+  def next = impl.next
+
+}
+
+object ClientWrapper {
+  private val clients = mutable.Map.empty[Credentials, Conjur]
+  def client(auth:Credentials) = clients.getOrElseUpdate(auth, new Conjur(new CachingAuthnProvider(auth)))
+
+  implicit def ClientWrapperToClient(cv:ClientWrapper) = cv.client
+  implicit def ClientWrapperToCredentials(cv:ClientWrapper) = cv.credentials
+  implicit def UserToClientWrapper(user:User) = ClientWrapper(user.getLogin, user.getApiKey)
+  implicit def CredentialsToClientWrapper(creds:Credentials) = ClientWrapper(creds)
+
+  def apply(creds:Credentials):ClientWrapper = new ClientWrapper(creds)
+  def apply(login:String, pswd:String):ClientWrapper = apply(new Credentials(login,pswd))
+
+}
+class ClientWrapper(val credentials:Credentials) {
+  def client = ClientWrapper.client(credentials)
+  def login  = credentials.getUsername
+  def withPassword(pw:String) = ClientWrapper(login, pw)
+}
+
+
+trait ConjurFixtures extends SuiteMixin { this: Suite =>
+
+  private val users = mutable.Map.empty[String, ClientWrapper]
+
+  private var _ns:String = null
+
+  abstract override def withFixture(test:NoArgTest) = {
+    _ns = admin.variables.createId
+    super.withFixture(test)
+  }
+
+  def ns:String = _ns
+  def ns(s:String):String = if(s.startsWith(ns)) s else ns + "-" + s
+  def ns(s:Symbol):String = ns(s.toString)
+
+  def admin : ClientWrapper = Credentials.fromSystemProperties
+
+  def createUser(login:String):User = {
+    val user = admin.users create ns(login)
+    users += login -> user
+    user
+  }
+
+  def createUser(login:String, password:String):User = {
+    val user = admin.users.create(ns(login), password)
+    users += login -> ClientWrapper(login, password)
+    user
+  }
+
+  def loginAs(name:String):ClientWrapper = users.getOrElse(name, createUser(name))
+  def loginAs(name:Symbol):ClientWrapper = loginAs(name.toString)
+
+
+  def randomUsername = RandomString("login-", 6) ++
+  def randomPassword = RandomString() ++
 }
--- src/test/scala/net/conjur/api/integration/UserFeatures.scala
+++ src/test/scala/net/conjur/api/integration/UserFeatures.scala
@@ -1,65 +1,140 @@
 package net.conjur.api.integration
-
-import net.conjur.api.{Credentials, Conjur}
-import org.scalatest.{GivenWhenThen, ShouldMatchers, FlatSpec}
-import net.conjur.api.specs.support.{RandomId, ResourceAuthnBehaviors}
 import scala.util.Random
-import net.conjur.api.authn.AuthnClient
+import org.scalatest._
+import net.conjur.api.authn._
+import net.conjur.api._
+import System.out.println
+
+case class UserWithPassword(user:User, password:String){
+  def this(user:User) = this(user, user.getApiKey)
+  def login = user.getLogin
+}
+object UserWithPassword{
+  def apply(user:User):UserWithPassword = new UserWithPassword(user)
+  implicit def userToUserWithPassword(user:User) = UserWithPassword(user)
+  implicit def userWithPasswordToCredentials(uwp:UserWithPassword) = new Credentials(uwp.user.getLogin, uwp.password)
+}
 
 /**
  *
  */
-class UserFeatures extends FlatSpec
+class UserFeatures extends FeatureSpec
     with ShouldMatchers
-    with ResourceAuthnBehaviors
     with ConjurFixtures
     with GivenWhenThen {
 
-  lazy val userName = new RandomId("java-api-test-user", 10)
+  info("As a Conjur based service")
+  info("I want to create Conjur users")
+  info("in order to represent my own users")
+  feature("User creation"){
+    scenario("Create and authenticate as a user without a password"){
+      When("I create a user without a password")
+      val alice = admin.users.create(ns("alice"))
 
-  def newUserClient() = {
-    val user= admin.users.create(userName ++)
-    new Conjur(user.getLogin, user.getApiKey)
+      def authn = new AuthnClient(alice.getLogin, alice.getApiKey)
+      Then("I can authenticate with the user's api key")
+      authn.authenticate
+
+      And("I can login with her api key")
+      authn.login should equal(alice.getApiKey)
+    }
+
+    scenario("Create and authenticate as a user with a password"){
+      When("I create a user with a password")
+      val pw = randomPassword
+      val alice = admin.users.create(ns("alice"), pw)
+      def authn(auth:String) = new AuthnClient(alice.getLogin, auth)
+
+      Then("I can authenticate with her api key")
+      authn(alice.getApiKey).authenticate
+
+      And("I can authenticate with her password")
+      authn(pw).authenticate
+
+      And("I can login with her api key")
+      authn(alice.getApiKey).login
+
+      And("I can log in with her password")
+      authn(pw).login
+
+    }
   }
 
-  "The admin client" should behave like resourceWithValidAuthn(admin)
+  info("As a conjur based service")
+  info("I want to check whether a user exists")
+  info("in order to tell users if a username is available")
+  feature("User existence testing"){
+    scenario("Created users exist"){
+      Given("a user I created")
+      val alice = admin.users.create(ns("alice"))
 
-  "An admin" should "be able to create a user" in {
-    val user = admin.users.create(userName ++)
+      Then("the user exists")
+      admin.users.exists(alice.getLogin) should be(true)
+    }
+
+    scenario("The admin user exists"){
+      Then("the user admin exists")
+      admin.users.exists(admin.login) should be(true)
+    }
+
+    scenario("A random username should not exist"){
+      Given("A random username")
+      val username = randomUsername
+      Then("no user exists with the name")
+      admin.users.exists(username) should be(false)
+    }
   }
 
-  "A client for a created user" should "be able to authenticate" in {
-    val client = newUserClient
-    client.getAuthn.authenticate
+  feature("API key access"){
+    scenario("Exchange a password for an API key"){
+      Given("A user named alice with a random password")
+      val pw = randomPassword
+      val alice = admin.users.create(ns('alice), pw)
+
+      Then("I can get her api key using her password")
+      new AuthnClient(alice.getLogin, pw).login should equal(alice.getApiKey)
+    }
   }
 
-  "A created user" should "have an apiKey" in {
-    admin.users.create(userName ++).getApiKey should not be(null)
+  feature("Password updates"){
+    scenario("Change a user's password using her old password"){
+      Given("A user with a password")
+      val password = randomPassword
+      val user = admin.users.create(randomUsername, password)
+
+      When("I authenticate using her username and original password")
+      val client = new AuthnClient(user.getLogin, password)
+
+      And("I generate a new random password")
+      val newPassword = randomPassword
+
+      And("I update her password to the new password")
+      client.updatePassword(newPassword)
+
+      Then("I can authenticate using the new password")
+      new AuthnClient(user.getLogin, newPassword).authenticate
+
+      And("I can login using the new password")
+      new AuthnClient(user.getLogin, newPassword).login
+    }
+    scenario("Change a user's password using her api key"){
+      Given("a user named alice")
+      val alice = loginAs('alice)
+
+      And("a random password")
+      val password = randomPassword
+
+      When("I update her password to the new password")
+      new AuthnClient(alice.getUsername, alice.getPassword).updatePassword(password)
+
+      Then("I can authenticate with the new password")
+      new AuthnClient(alice.getUsername, password).authenticate
+    }
   }
 
-  "A user" should "be able to change her password and login with the new one" in {
-    Given("a random password")
-    val password = Random.nextString(10)
 
-    When("I create a user with the password")
-    val user = admin.users.create(userName ++, password)
 
-    And("I create an authn client with the username and password")
-    val authn = new AuthnClient(user.getLogin, password)
 
-    Then("the authn client can authenticate with the old password")
-    authn.authenticate
-
-    And("it can change the user's password")
-    val newPassword = Random.nextString(10)
-    authn.updatePassword(newPassword)
-
-    When("I create an authn client with the new password")
-    val newAuthn = new AuthnClient(user.getLogin, newPassword)
-
-    Then("it can authenticate")
-    newAuthn.authenticate
-  }
 
 
 }
