b376a1849d052378b404ef43b2c675f077aa880f
Jon <jonathan.j.mason@gmail.com>
2013-10-03 16:49:05 -0500

    equals and hashCode for Credentials

--- src/main/java/net/conjur/api/Credentials.java
+++ src/main/java/net/conjur/api/Credentials.java
@@ -38,4 +38,23 @@ public class Credentials {
         return new Credentials(parts[0], parts[1]);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Credentials)) return false;
+
+        Credentials that = (Credentials) o;
+
+        if (!password.equals(that.password)) return false;
+        if (!username.equals(that.username)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
 }
