f420b7dbba20b1e5a559d2ffc47b423dfb17f836
Jonathan Mason <jonathan.j.mason@gmail.com>
2015-11-03 15:59:52 -0600

    dockerize api-java build

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# we can build the image every time without wasting too much time
+docker build -t conjur-api -f Dockerfile.jenkins .
+
+mkdir -p $PWD/target
+
+docker run -t \
+    -v $PWD/target:/build/target \
+    conjur-api:latest
+
+
