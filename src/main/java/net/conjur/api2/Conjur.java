8386232bf13b7dde690899205c721baa6d066d8b
Oren Ben Meir <oren.benmeir@cyberark.com>
2017-07-26 16:29:24 +0300

    - Added accountName to credentials - Added some templates to the initial skeleton

--- src/main/java/net/conjur/api2/Conjur.java
+++ src/main/java/net/conjur/api2/Conjur.java
@@ -2,31 +2,62 @@ package net.conjur.api2;
 
 
 /**
- *
+ * Entry point for the Conjur API client.
  */
 public class Conjur {
 
     private static Conjur instance;
 
     private Credentials credentials;
+    private String token;
 
     private Conjur(){
         credentials = Credentials.fromSystemProperties();
     }
 
+    /**
+     * @return an instance of Conjur, ready for storing and retrieving secrets
+     */
     public static Conjur getInstance() {
         if (instance == null) {
             instance = new Conjur();
         }
 
-        // login
-
-        // authenticate
+        if (!isTokenValid()) {
+            login();
+            authenticate();
+        }
 
         return instance;
     }
 
-    public String getSecret() {
-        return "changeit";
+    private static void login() {
+        // use hostname, apiKey, url & accountName from credentials object (credentials.hostname etc.)
+
+        // token=$(curl --user admin:3j9v7d52s8q2jz35pvz4he01c61ghsrfj3xm9s893ay1vks60hhrw http://possum:3000/authn/test/login)
+    }
+
+    private static void authenticate() {
+        // use token (retrieved in login method)
+        // use hostname, apiKey, url & accountName from credentials object (credentials.hostname etc.)
+
+        // response=$(curl --data "$login_token" http://possum:3000/authn/test/admin/authenticate)
+        // token=$(echo -n $response | base64 | tr -d '\r\n')
+    }
+
+    private static boolean isTokenValid() {
+        // check with server if the token received in last login is still valid
+        return true;
+    }
+
+    public String getSecret(String secretKey) {
+        // use token (retrieved in login method)
+
+        // curl -H "Authorization: Token token=\"$token\"" http://possum:3000/secrets/test/variable/test-secret
+        return null;
+    }
+
+    public void setSecret(String secretKey, String secretValue){
+        // curl -H "Authorization: Token token=\"$token\"" --data "super-secret" http://possum:3000/secrets/test/variable/test-secret
     }
 }
--- src/main/java/net/conjur/api2/Credentials.java
+++ src/main/java/net/conjur/api2/Credentials.java
@@ -6,9 +6,10 @@ public class Credentials {
     private String hostname;
     private String apiKey;
     private String url;
+    private String accountName;
     // todo: add CA Cert?
 
-    public Credentials(String hostname, String apiKey, String url) {
+    public Credentials(String hostname, String apiKey, String url, String accountName) {
         this.hostname = hostname;
         this.apiKey = apiKey;
         this.url = url;
@@ -17,7 +18,7 @@ public class Credentials {
     public static Credentials fromSystemProperties(){
         String credentials = System.getProperty(CREDENTIALS_PROPERTY);
         String[] parts = credentials.split(":"); // Assuming credentials are written in the systemParams in this way: "hostname:apiKey:url"
-        return new Credentials(parts[0], parts[1], parts[2]);
+        return new Credentials(parts[0], parts[1], parts[2], parts[3]);
     }
 
     public String getHostname() {
@@ -30,6 +31,10 @@ public class Credentials {
 
     public String getUrl() {
         return url;
+    }
+
+    public String getAccountName() {
+        return accountName;
     }
 
 }
