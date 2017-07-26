1fde45e0b293011f4150f4b03d7bad505ff4e075
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-26 18:04:53 +0300

    added api key generation and cleanup upon exit

--- run.sh
+++ run.sh
@@ -4,15 +4,11 @@ function finish {
 	docker rm -f $pg_cid
 	docker rm -f $server_cid
 }
-# trap finish EXIT
+trap finish EXIT
 
 export POSSUM_DATA_KEY="$(docker run --rm possum data-key generate)"
 
 pg_cid=$(docker run -d postgres:9.3)
-
-# mkdir -p spec/reports
-# mkdir -p cucumber/api/features/reports
-# mkdir -p cucumber/policy/features/reports
 
 server_cid=$(docker run -d \
 	--link $pg_cid:pg \
@@ -20,6 +16,18 @@ server_cid=$(docker run -d \
 	-e RAILS_ENV=development \
   -p 3000:80 \
 	possum server -a cucumber)
+
+sleep 10
+
+api_key=$(docker exec "$server_cid" rails r "print Credentials['cucumber:user:admin'].api_key")
+echo '!!!!!!'
+echo ''
+echo "API Key: $api_key"
+echo ''
+echo '!!!!!!'
+
+docker exec -it "$server_cid" tail -f /opt/possum/log/development.log
+
 
 # cat << "TEST" | docker run \
 # 	-i \
