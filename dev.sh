d0fe85a4d248ea63ae4c4f46b8d09638f0f23151
AndrewCopeland <andcope1995@gmail.com>
2020-02-03 14:01:27 -0500

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,29 +1,16 @@
-#!/bin/bash -ex
-
-function finish {
-	docker rm -f $pg_cid
-	docker rm -f $server_cid
-}
+#!/bin/bash -e
+source utils.sh
 trap finish EXIT
 
-export POSSUM_DATA_KEY="$(docker run --rm possum data-key generate)"
+createOssTestEnvironment
+loadOssTestPolicy
 
-pg_cid=$(docker run -d postgres:9.3)
-
-server_cid=$(docker run -d \
-	--link $pg_cid:pg \
-	-e DATABASE_URL=postgres://postgres@pg/postgres \
-	-e RAILS_ENV=development \
-  -p 3000:80 \
-	possum server -a cucumber)
-
-sleep 10
-
-api_key=$(docker exec "$server_cid" rails r "print Credentials['cucumber:user:admin'].api_key")
+service_cid="conjur-api-java_conjur_1"
+api_key=$(docker exec $service_cid conjurctl role retrieve-key cucumber:user:admin)
 echo '!!!!!!'
 echo ''
 echo "API Key: $api_key"
 echo ''
 echo '!!!!!!'
 
-docker exec -it "$server_cid" tail -f /opt/possum/log/development.log
+docker logs --tail 0 -f "$service_cid"
