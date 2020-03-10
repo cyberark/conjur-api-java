6d45558e2775e5a65811b226cac1e5e588866313
Hadar Artzi <hadar.artzi@gmail.com>
2020-03-10 13:52:53 -0400

    add clean

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 export VERSION=$(./get-version.sh)
 
 docker run --rm -v "$PWD:/conjurinc/api-java" -e VERSION -w /conjurinc/api-java maven:3-jdk-8 /bin/bash -c \
-"mvn -X -e package -Dmaven.test.skip=true"
+"mvn -X -e clean package -Dmaven.test.skip=true"
