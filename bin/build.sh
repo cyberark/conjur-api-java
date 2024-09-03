e4184bd9f01fd5f0cb63402adda6297d8d08da3e
Shlomo Heigh <shlomo.heigh@cyberark.com>
2024-09-03 14:30:55 -0400

    CNJR-5559: Migrate to automated release process

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-docker run --rm \
-  -v "$PWD:/cyberark/conjur-java-api" \
-  -w /cyberark/conjur-java-api maven:3-jdk-8 \
-  /bin/bash -ec "mvn -X -e clean package -Dmaven.test.skip=true"
